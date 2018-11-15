6dc6cb546321b471bd4c63bd14f1f86dac211cd6
Ladrillo <gabriel@cabrejas.net>
2018-11-15 20:03:40 +0100

    get linting working

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true,
+    },
+    "rules": {
+        "camelcase": 0,
+        "object-curly-newline": 0,
+        "arrow-parens": 0,
+    }
+};
