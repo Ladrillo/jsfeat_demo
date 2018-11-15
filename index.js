aa51d52355da75ba95ea04e6b8483e4e8eda3602
Ladrillo <gabriel@cabrejas.net>
2018-11-15 20:06:47 +0100

    start camera

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 window.onload = () => {
   const video = document.querySelector('#vid');
+
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  })
+    .then(stream => {
+      video.srcObject = stream;
+      video.play();
+    });
 };
